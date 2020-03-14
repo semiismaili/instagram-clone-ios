25b100a3df0da3c4eb79647ec4461bc55483c36a
Semi Ismaili <semiismaili@Semis-Mac.local>
2020-03-14 15:48:29 +0100

    Initial Commit

---
+++ instagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by Semi Ismaili on 3/14/20.
+//  Copyright © 2020 Semi Ismaili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
